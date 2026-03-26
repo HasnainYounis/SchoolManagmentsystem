3a923e7283a8bcbb6676a487fb2fcc8602a1d409
Hasnain younis <hasnain.younis765@gmail.com>
2026-03-26 23:39:25 +0500

    new file in main

---
+++ pro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $cars = ["mehran","civic","sonata"];
+foreach($cars as $car){
+    echo $car."<br>";
+}
+    ?>
+</body>
+</html>
